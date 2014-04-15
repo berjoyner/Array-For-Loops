9aa37aee8d8fc14566d674f7a71ee83e47fe7365
Berjoyner <joyner.amber@gmail.com>
2014-04-14 23:56:14 -0400

    For Loop and Each

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@
 //            markup += "<li>" + data[i] + "</li>";
            
 //          }
- 
+
 //          // console.log(markup);
  
 //          return markup;
@@ -21,7 +21,7 @@
  
    // $(".cats").append(listItemTemplate(catsArray));
 
-   var navArray = ["<a href=home.html>Home</a>" , "<a href=about.html>About</a>" , "<a href=travel.html>Travel</a>" , "<a href=blog.html>Blog</a>" , "<a href=contact.html>Contact</a>"];
+   var navArray = ["<a href=index.html>Home</a>" , "<a href=about.html>About</a>" , "<a href=travel.html>Travel</a>" , "<a href=blog.html>Blog</a>" , "<a href=contact.html>Contact</a>"];
 
    var navTemplate = function(navData) {
 
@@ -37,3 +37,35 @@
    };
 
    $(".nav").append(navTemplate(navArray));
+
+
+var mediaArray = ["<a href=http://twitter.com><img src=/images/twitter.png></a>" , 
+				"<a href=http://facebook.com><img src=/images/facebook.png></a>" , 
+				"<a href=http://reddit.com><img src=/images/reddit.png></a>" , 
+				"<a href=http://google.com><img src=/images/google.png></a>" , 
+				"<a href=http://ambernjoyner.wordpress.com><img src=images/wordpress.png></a>"];
+
+var mediaTemplate = function(mediaData) {
+
+	var markup ="";
+	var i; 
+
+	for (i=0; i<mediaData.length; i++) {
+		markup += mediaData[i];
+	}
+
+	return markup;
+	//console.log(markup);
+
+};
+
+$(".mednav").append(mediaTemplate(mediaArray));
+
+
+
+
+$(".jumbotron").each( function(index) {
+
+    $(this).css("background-color", "#BDEDFF");
+});
+
